28e9b83c972423f3ec87535a519a60a7e147fa82
unknown <vathroh@gmail.com>
2022-06-05 11:02:57 +0700

    job finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className='box'>
+        <h2>Login</h2>
+        <form>
+          <div className='input-group'>
+            <input type="text" required/>
+            <label>Username</label>
+          </div>
+          <div className='input-group'>
+            <input type="password" required/>
+            <label>Password</label>
+          </div>
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
     </div>
   );
 }
